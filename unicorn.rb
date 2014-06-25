0fb8551d704d0e9fb62e474a9357d2fb8b309c8d
Zi <hungryzi@gmail.com>
2014-06-26 01:50:36 +0800

    Colored rainbow

--- unicorn.rb
+++ unicorn.rb
@@ -6,12 +6,28 @@ def rainbow
   5.times do |row|
     terminal_width.times do |col|
       if col.odd?
-        print '-'
+        print rainbowify("_", col)
       else
-        print '_'
+        print rainbowify("-", col)
       end
     end
     print "\n"
+  end
+end
+
+def rainbowify(string, col)
+  c = colors[col % colors.size]
+  "\e[38;5;#{c}m#{string}\e[0m"
+end
+
+def colors
+  @colors ||= (0...(6 * 7)).map do |n|
+    pi_3 = Math::PI / 3
+    n *= 1.0 / 6
+    r  = (3 * Math.sin(n           ) + 3).to_i
+    g  = (3 * Math.sin(n + 2 * pi_3) + 3).to_i
+    b  = (3 * Math.sin(n + 4 * pi_3) + 3).to_i
+    36 * r + 6 * g + b + 16
   end
 end
 
