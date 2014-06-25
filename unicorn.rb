ec35ab08e51a2da62ccf66c11bbfced5a7764c45
Zi <hungryzi@gmail.com>
2014-06-26 01:25:03 +0800

    White rainbow

---
+++ unicorn.rb
@@ -0,0 +1,18 @@
+def terminal_width
+  @terminal_width ||= `stty size`.split.map { |x| x.to_i }.reverse.first - 1
+end
+
+def rainbow
+  5.times do |row|
+    terminal_width.times do |col|
+      if col.odd?
+        print '-'
+      else
+        print '_'
+      end
+    end
+  end
+end
+
+rainbow
+
